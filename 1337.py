f47999dfd150c28e8dab4519e3232cf27cd92968
Håkan Waara <hwaara@gmail.com>
2013-10-30 10:59:41 +0100

    Add code for the1337bot, originally written 2 years ago with a slight update recently

---
+++ 1337.py
@@ -0,0 +1,51 @@
+#!/usr/local/bin/python2.6
+# encoding: utf-8
+"""
+1337.py
+Written October 29, 2011 by Håkan Waara (hwaara@gmail.com)
+If you use this for something fun, let me know.
+"""
+import os
+import sys
+import datetime
+import tweepy
+import random
+
+test = "test" in sys.argv
+
+# only run about once every day
+if datetime.datetime.now().strftime("%H%M") != "1337":
+    print "The time is not 13.37!"
+    if not test:
+        sys.exit(1)
+else:
+    print "13.37 ... let's tweet this!"
+
+# login
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN_KEY'], os.environ['ACCESS_TOKEN_SECRET'])
+
+api = tweepy.API(auth)
+
+followers = dict([(user.id, user) for user in api.followers()])
+friends = dict([(user.id, user) for user in api.friends()])
+
+# follow people following us
+for (user_id, user) in followers.iteritems():
+    if user_id in friends:
+        # already followed
+        del friends[user_id]
+    else:
+        print "Following user %s" % user.screen_name
+        user.follow()
+
+# unfollow people not following us
+for user in friends:
+    print "Unfollowing user %s" % user.screen_name
+    user.unfollow()
+
+message = random.choice(["13.37", "13.37!", "1337", "13.37", "1337!"])
+
+print "Tweeting"
+if not test:
+    api.update_stats(message)
